d4c2171db3903960ab67596e2031d0f63dd1b06f
Wildan Kevin <wldnkvny@gmail.com>
2025-10-14 14:11:44 +0700

    Update product catalog with new items
    Added new products to the catalog in Main.java.

--- ETS/code/Main.java
+++ ETS/code/Main.java
@@ -2,14 +2,16 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        // Seed katalog awal
         ArrayList<Produk> katalog = new ArrayList<>();
         katalog.add(new Produk("Keripik Kentang", "Snack", 12000, 10));
-        katalog.add(new Produk("Cokelat Bar", "Snack", 10000, 8));
+        katalog.add(new Produk("Cokelat", "Snack", 10000, 8));
         katalog.add(new Produk("Biskuit", "Snack", 8000, 12));
+        katalog.add(new Produk("Roti Coklat", "Snack", 6000, 10));
+        katalog.add(new Produk("Sosis", "Snack", 5000, 16));
         katalog.add(new Produk("Air Mineral", "Minuman", 6000, 15));
         katalog.add(new Produk("Teh Botol", "Minuman", 9000, 10));
         katalog.add(new Produk("Soda", "Minuman", 11000, 7));
+        katalog.add(new Produk("Susu Sapi", "Minuman", 12000, 12));
 
         // Controller utama
         MonsterMart app = new MonsterMart(katalog);
